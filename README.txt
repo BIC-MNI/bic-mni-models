87129033002a2c5bad0d931a6a12d473a4994f0d
Vladimir S. FONOV <vladimir.fonov@gmail.com>
2012-04-20 17:11:23 -0400

    Added initial version

---
+++ README.txt
@@ -0,0 +1,5 @@
+
+ Collection of anatomical templates commonly used for image processing with minc tools
+ 
+ For details see http://www.bic.mni.mcgill.ca/ServicesAtlases/HomePage
+ 
---
+++ cmake-modules/DownloadTemplate.cmake
@@ -0,0 +1,19 @@
+function(DOWNLOAD_TEMPLATE url md5 dst )
+  file(DOWNLOAD "${url}" "${dst}"  EXPECTED_MD5  "${md5}" SHOW_PROGRESS )
+ENDFUNCTION(DOWNLOAD_TEMPLATE)
+
+function(UNPACK_AND_INSTALL_TEMPLATE archive name)
+  add_custom_target( 
+    unpack_${name} ALL
+    unzip -f -o ${archive} -d ${CMAKE_CURRENT_BINARY_DIR}/${name}
+    DEPENDS ${archive}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Unpacking ${name}" VERBATIM
+  )
+
+  INSTALL(DIRECTORY 
+      ${CMAKE_CURRENT_BINARY_DIR}/${name}
+  DESTINATION 
+      share/)
+
+ENDFUNCTION(UNPACK_AND_INSTALL_TEMPLATE)
